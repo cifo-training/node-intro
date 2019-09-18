51450e8db2fd427db15f62eb6e4f8166a4d99123
unknown <cjordanvergara@outlook.es>
2019-09-18 12:22:46 +0200

    Final Carlos-Dani

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,12 +1,36 @@
 class SortedList {
+    constructor(){
+        this.items=[];
+        this.length=0;
+    }
 
-
-    add(item) {}
-    get(pos) {}
-    max() {}
-    min() {}
-    average() {}
-    sum() {}
+    add(item) {
+        this.items.push(parseInt(item));
+        this.items.sort((a, b)=>{
+            return a - b;
+          });
+        this.length=this.items.length;
+    }
+    get(pos) {
+        return this.items[pos-1];
+    }
+    max() {
+        return this.items[this.length-1];
+    }
+    min() {
+        return this.items[0];
+    }
+    average() {
+        try {
+            return this.sum()/this.length;
+        } catch (error) { }
+        
+    }
+    sum() {
+        if (this.length>0){
+            return this.items.reduce((a,b)=>a+b);
+        }
+    }
 }
 
 module.exports = SortedList;

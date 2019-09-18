7a7d05930dc6fbc3f3e475ab0e40c4eb52612ce1
kifka81 <angelaturizopinzon@gmail.comgit config --global user.email angelaturizopinzon@gmail.comgit config --global user.email angelaturizopinzon@gmail.com>
2019-09-18 12:21:20 +0200

    primer commit

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,11 +1,19 @@
 class SortedList {
-
-    add(item) {}
+    constructor() {
+        this.length = 0;
+        this.items = [];
+    }
+    add(item) {
+        let item = []; 
+        for(let i=1; i<=30; i++){
+        item.push(i);   
+        }
+    }
     get(pos) {}
     max() {}
     min() {}
     average() {}
     sum() {}
 }
-
+ 
 module.exports = SortedList;

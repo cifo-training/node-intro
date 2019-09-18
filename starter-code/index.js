7d9c75adfc2156c05141d4210966eb47b575fdb3
HectorCo <hcompanyortega@gmail.com>
2019-09-18 16:38:49 +0200

    todo ok menos un test del método get

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,62 +1,74 @@
-
 class SortedList {
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
+  add(x) {
+    this.items.push(x);
+    this.items.sort();
+    this.length++;
+  }
 
-    constructor () {
-       this.items = [];
-       this.length = 0;
-      }
+  get(pos) {
 
-    add(x) {
-       this.items.push(x);
-       this.items.sort();
-       this.length++;
-
-    }
-
-    get(pos) {
-        
     return this.items[pos-1];
+  }
 
-    }
-
-
-    max() {
-        
-    return this.items[this.items.length -1];
-        
-
-    }
-    min() {
+  max() {
+    return this.items[this.items.length - 1];
+  }
+  min() {
     return this.items[0];
+  }
+  average() {
+    let suma = 0;
 
+    if (this.items.length == 0) {
+      suma = 0;
+    } else {
+      suma =
+        this.items.reduce(function(a, b) {
+          return a + b;
+        }) / this.length;
     }
-    average() {}
-    sum() {}
-}
 
+    return suma;
+  }
+
+  sum() {
+    let suma = 0;
+
+    if (this.items.length == 0) {
+      suma = 0;
+    } else {
+      suma = this.items.reduce(function(a, b) {
+        return a + b;
+      });
+    }
+
+    return suma;
+  }
+}
 
 var lista = new SortedList();
 
-
- lista.add(4);
- lista.add(5);
- lista.add(1);
- lista.add(6);
+lista.add();
+lista.add();
+lista.add();
+lista.add();
 
 var posicion = lista.get(1);
 var maximo = lista.max();
 var minimo = lista.min();
+var media = lista.average();
 
 console.log(lista.length);
 console.log(posicion);
 console.log(maximo);
 console.log(minimo);
+console.log(media);
 
 console.log(lista);
 
-
 module.exports = SortedList;
-
-
-

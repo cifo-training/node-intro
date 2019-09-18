725dfb8e10ac0993f78ad92fd978b8791a3d6e4b
unknown <armandrodgo@gmail.com>
2019-09-18 12:22:40 +0200

    Start test - 9 passed

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,7 +1,33 @@
 class SortedList {
 
-    add(item) {}
-    get(pos) {}
+    constructor (){
+        this.items = [];
+        this.length = 0;
+    }
+
+    
+    add(item) {
+        //console.log(item);
+        //console.log(item);
+        //this.items.push(item);
+        
+        this.length = this.items.length;
+        
+        this.items[this.length] = item; 
+        //this.items.push(item);
+        this.items.sort();
+        this.length++;
+        
+               
+        //this.length += 1;
+        
+        //console.log(item);
+    }
+    get(pos) {
+        pos = pos-1;
+        return this.items[pos];
+    }
+
     max() {}
     min() {}
     average() {}
@@ -9,3 +35,24 @@ class SortedList {
 }
 
 module.exports = SortedList;
+
+SortedList = new SortedList();
+
+SortedList.add(52);
+console.log(SortedList.length);
+console.log(SortedList.items);
+
+SortedList.add(42);
+console.log(SortedList.length);
+console.log(SortedList.items);
+
+SortedList.add(48);
+console.log(SortedList.length);
+console.log(SortedList.items);
+
+SortedList.get(1);
+console.log(SortedList.get(1));
+SortedList.get(2);
+console.log(SortedList.get(2));
+SortedList.get(3);
+console.log(SortedList.get(3));

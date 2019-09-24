2acd0115a774aaf10a4bad3c1e3aff75e3cdbdb9
ArmandRodGo <armandrodgo@gmail.com>
2019-09-24 12:56:03 +0200

    done

--- starter-code/index.js
+++ starter-code/index.js
@@ -1,68 +1,59 @@
-class SortedList {
+class SortedList {   
 
     constructor (){
-        this.items = [];
-        this.length = 0;
-    }
-
+    this.items = [];
+    this.length = 0;
+    }    
     
     add(item) {
-        //console.log(item);
-        //console.log(item);
-        //this.items.push(item);
-        
+        this.items.push(item);
+        this.items.sort((a,b)=>a-b);
         this.length = this.items.length;
-        
-        this.items[this.length] = item; 
-        //this.items.push(item);
-        this.items.sort();
-        this.length++;
-        
-               
-        //this.length += 1;
-        
-        //console.log(item);
-    }
+    }    
+    
     get(pos) {
-        //console.log(pos);
         pos = pos-1;
-        //console.log(typeof(this.items[pos]));
-        //console.log(pos);
-        //console.log(this.items[pos]);
         return this.items[pos];
-        
-    }
+    }    
 
     max() {
-        //console.log("x:"+ this.max());
-        return this.items.max();
-        
+        let mayor=this.items[0];
+        for(let i=0; i<(this.items.length); i++){
+            if (this.items[i]>mayor){
+                mayor=this.items[i];
+            }
+        }
+        return mayor;
+    }    
+    
+    min() {
+    let menor=this.items[0];
+    for(let i=0; i<(this.items.length); i++){
+        if (this.items[i]<menor){
+            menor=this.items[i];
+        }
     }
-    min() {}
-    average() {}
-    sum() {}
+    return menor;
+    }    
+
+    average() {
+        let media=0;
+        let contador=0;
+        for(let i=0; i<(this.items.length); i++){
+            contador+=this.items[i]
+        }
+    media=contador/this.items.length;
+    return media;
+    }    
+
+    sum() {
+    let total=0;
+    for(let i=0; i<(this.items.length); i++){
+        total+=this.items[i]
+    }
+    return total;
+    }
 }
 
 module.exports = SortedList;
-
-//SortedList = new SortedList();
-
-/*SortedList.add(52);
-console.log(SortedList.length);
-console.log(SortedList.items);
-
-SortedList.add(42);
-console.log(SortedList.length);
-console.log(SortedList.items);
-
-SortedList.add(48);
-console.log(SortedList.length);
-console.log(SortedList.items);
-
-SortedList.get(1);
-console.log(SortedList.get(1));
-SortedList.get(2);
-console.log(SortedList.get(2));
-SortedList.get(3);
-console.log(SortedList.get(3));
-*/
+SortedList = new SortedList();
